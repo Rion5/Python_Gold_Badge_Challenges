e32b1c53107080f94fe03f55125838f0c3ed953f
Rion5 <31965265+Rion5@users.noreply.github.com>
2019-02-22 14:46:17 -0500

    Completed Challenge 4

--- challenge_4/badge_repository.py
+++ challenge_4/badge_repository.py
@@ -1,6 +1,5 @@
 from badge import Badge
 # from challenge_4.badge import Badge
-# from challenge_4.badge_repository import BadgeRepository
 class BadgeRepository:
     def __init__(self, badges_list: list):
         self.badges_list = badges_list
@@ -9,6 +8,24 @@ class BadgeRepository:
     def add_badge(self, badge: Badge):
         self.badges_list.append(badge)
 
+    # Remove Door from List
+    def remove_door(self, access_code_to_remove):
+        for i, badge in enumerate(self.badges_list):
+            for j, access_code in enumerate(badge.list_of_door_names):
+                if access_code == access_code_to_remove:
+                    index_to_del = self.badges_list[i].list_of_door_names.index(self.badges_list[i].list_of_door_names[j])
+                    # Remove the access door code from the list_of_door_names from inside the Badge class
+                    self.badges_list[i].list_of_door_names.remove(self.badges_list[i].list_of_door_names[j])
+
+    # Append Door to list
+    def add_door(self, badge_number, new_access_code):
+        # Append the access code to the correct badge
+        for i, badge in enumerate(self.badges_list):
+            if badge.badge_number == badge_number:
+                self.badges_list[i].list_of_door_names.append(new_access_code)
+                print('adding')
+                return
+    
     # View All
     def view_all_badges(self):
         print('Badge #\t\t\tDoor Access')
--- challenge_4/ui.py
+++ challenge_4/ui.py
@@ -2,7 +2,6 @@ from badge import Badge
 from badge_repository import BadgeRepository
 # from challenge_4.badge import Badge
 # from challenge_4.badge_repository import BadgeRepository # For code completion
-
 class UI:
     def __init__(self, badges_repository: BadgeRepository):
         self.badge_repo = badges_repository
@@ -15,11 +14,13 @@ class UI:
         "\nPress any other key to exit()")
         user_input = input('Select an option: ')
         return user_input
+
     def create_badge_prompt(self):
         badge_number  = input('What is the number on the badge: ')
         door_access_codes = self.add_door_access_prompt()
         new_badge = Badge(badge_number, door_access_codes)
         self.badge_repo.add_badge(new_badge)
+
     def add_door_access_prompt(self):
         door_access_codes = []
         result = True
@@ -32,15 +33,26 @@ class UI:
             else:
                 result = False
         return door_access_codes
+
     def edit_badge_prompt(self):
         badge_number_to_edit = input('What is the badge number to update?: ')
         for i, badge in enumerate(self.badge_repo.badges_list):
             if badge.badge_number == badge_number_to_edit:
-                print(f'{badge.badge_number} has access to doors {badge.list_of_door_names}')
-                print('What would you like to do?')
+                print(f'\n{badge.badge_number} has access to doors {badge.list_of_door_names}')
+                print('\nWhat would you like to do?')
                 print('\t1. Remove a door')
                 print('\t2. Add a door')
-    
+                user_input = input('Select an option: ')
+                if user_input == "1":
+                    access_code_to_remove = input('Which door access would you like to remove?: ')
+                    self.badge_repo.remove_door(access_code_to_remove)
+                elif user_input == "2":
+                    access_code_to_add = input('Which door access would you like to add?: ')
+                    self.badge_repo.add_door(badge.badge_number, access_code_to_add)
+                else:
+                    print('Error: Invalid Option')
+                    self.edit_badge_prompt()
+                
     def menu_option_logic(self, option_number: str):
         if option_number == "1":
             print('\nAdd a Badge')
